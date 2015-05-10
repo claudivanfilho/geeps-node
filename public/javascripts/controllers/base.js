86755a2e3e0dfcc5097e87f93cc2d00fe7b546c2
claudivanfilho <j.claudivan.filho@gmail.com>
2015-05-10 11:06:01 -0300

    remove trash files

--- public/javascripts/controllers/base.js
+++
@@ -1,6 +0,0 @@
-angular.module("Geeps", ['ui.bootstrap','ui.router','cgBusy'])
-
-.config(function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('##');
-    $interpolateProvider.endSymbol('##');
-})
