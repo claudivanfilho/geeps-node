606398bd2174f7446a8eca0f04bb7f710e6b4132
claudivanfilho <j.claudivan.filho@gmail.com>
2015-05-19 11:00:53 -0300

    testes de usuario| routa para verificar existencia de usuario criada

--- routes/index.js
+++ routes/index.js
@@ -9,46 +9,59 @@ var rest = require('connect-rest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname+'/../views/index.html'));
+    res.sendFile(path.join(__dirname+'/../views/index.html'));
 });
 
 router.get('/empresas', function(req, res, next) {
-  mongoose.model('Empresa').find(function(err, empresas) {
-  	res.send(empresas);
-  })
+    mongoose.model('Empresa').find(function(err, empresas) {
+        res.send(empresas);
+    })
 });
 
 router.get('/usuarios', function(req, res, next) {
-  mongoose.model('Usuario').find(function(err, usuarios) {
-  	res.send(usuarios);
-  })
+    mongoose.model('Usuario').find(function(err, usuarios) {
+        res.send(usuarios);
+    })
 });
 
 router.get('/usuario', function(req, res){
-  Usuario.find(function(err, usuarios){
-    if(err) return res.send(500, 'Error occurred: database error.');
-    res.json(usuarios.map(function(a){
-      return {
-        name: a.name,
-        phone: a.phone,
-        countryCode: a.countryCode,
-        regId: a.regId,
-      }
-    }));
-  });
+    Usuario.find(function(err, usuarios){
+        if(err) return res.send(500, 'Error occurred: database error.');
+        res.json(usuarios.map(function(a){
+            return {
+                name: a.name,
+                phone: a.phone,
+                countryCode: a.countryCode,
+                regId: a.regId
+            }
+        }));
+    });
 });
 
 router.post('/usuario/cadastro', function(req, res){
-  var a = new Usuario({
-    name: req.body.name,
-    phone: req.body.phone,
-    countryCode: req.body.countryCode,
-    regId: req.body.regId
-  });
-  a.save(function(err, a){
-    if(err) return res.send(500, 'Error occurred: database error.');
-    res.json();
-  });
+    var a = new Usuario({
+        name: req.body.name,
+        phone: req.body.phone,
+        countryCode: req.body.countryCode,
+        regId: req.body.regId
+    });
+    a.save(function(err, a){
+        if(err) return res.send(500, 'Error occurred: database error.');
+        res.json({'message' : 'usuario cadastrado com sucesso'});
+    });
+});
+
+router.post('/verifica_user', function(req, res){
+    var telefone = req.body.phone;
+    Usuario.find({
+        phone: telefone
+    },function(err, usuarios){
+        if (usuarios.length == 0) {
+            res.json({'erro' : 'usuario inexistente'});
+        } else {
+            res.json({'erro' : 'usuario cadastrado'});
+        }
+    });
 });
 
 module.exports = router;
---
+++ test/usuarioTest.js
@@ -0,0 +1,57 @@
+var assert = require("assert");
+var Usuario = require("../models/usuario");
+var mongoose = require("mongoose");
+//db = mongoose.connect('mongodb://localhost/geeps_test');
+
+describe('Usuario TEST', function(){
+
+    beforeEach(function(done){
+        // cria um usuario
+        Usuario.create({
+            name: "Joao",
+            phone: "99876534",
+            countryCode: "51",
+            regId: "aopdpaodspoajsdij1231ej1d09"
+        }, function(){
+            done();
+        });
+    })
+
+    afterEach(function(done){
+        // remove todos os registros do bd
+        Usuario.remove({}, function(){
+            done();
+        });
+    })
+
+    describe('TESTA CRIACAO DO USUARIO' , function(){
+        it('Precisa existir uma usuario no BD', function(done){
+            Usuario.find(function(err, usuarios){
+                assert.equal(1, usuarios.length);
+                done();
+            });
+        })
+    });
+
+    describe('TESTA EXISTE USUARIO COM DADO TELEFONE' , function(){
+        it('Precisa existir uma usuario com o dado telefone no BD', function(done){
+            Usuario.find({
+                phone: "99876534"
+            },function(err, usuarios){
+                assert.equal(1, usuarios.length);
+                done();
+            });
+        })
+    });
+
+    describe('TESTA SE NÃO EXISTE USUARIO COM DADO TELEFONE' , function(){
+        it('Não deve existir uma usuario com o dado telefone no BD', function(done){
+            Usuario.find({
+                phone: "4"
+            },function(err, usuarios){
+                assert.equal(0, usuarios.length);
+                done();
+            });
+        })
+    });
+})
