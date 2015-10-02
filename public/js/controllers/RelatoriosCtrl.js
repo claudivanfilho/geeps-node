dd7c2f4bd1d0e274084961e951ef7f657713ddc1
Túlio <tulio.figueiredo@gmail.com>
2015-10-02 11:03:27 -0300

    Tentando colocar os gráficos

--- public/js/controllers/RelatoriosCtrl.js
+++ public/js/controllers/RelatoriosCtrl.js
@@ -1,65 +1,65 @@
 angular.module("Geeps")
-    .controller('RelatoriosCtrl', ['Pedidos', RelatoriosController]);
+    .controller('RelatoriosCtrl', RelatoriosController);
 
-RelatoriosController.$inject = ['$scope'];
+RelatoriosController.$inject = ['$scope', 'Pedidos'];
 
 function RelatoriosController($scope, Pedidos) {
 
+    $scope.pedService = Pedidos;
+    if(Pedidos.pedidos.length == 0) {
+        Pedidos.refresh();
+    }
+
     var pedidos_concluidos = [];
-    $scope.pedidos.forEach(function(obj) {
+    $scope.pedService.pedidos.forEach(function(obj) {
         if (obj.status === "CONCLUÍDO") {
             pedidos_concluidos.push(obj);
         }
     });
 
     var mapBairros = new Map();
-    $scope.dados = [];
+    $scope.pedService.dados = [];
 
     var mapDatas = new Map();
-    $scope.datasDosPedidos = new Array();
+    $scope.pedService.datasDosPedidos = new Array();
 
-    $scope.initData = function() {
-        console.log("initData entrou");
-        
-        pedidos_concluidos.forEach(function(obj) {
-            
-            // ---------------------------
-            // Gráfico utilizando bairro - cidade
-            // ---------------------------
-            if (mapBairros.get(obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade)) {
-                var cont = mapBairros.get(obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade);
-                mapBairros.delete(obj.endereco_entrega.bairro);
-                mapBairros.set(obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade, cont + 1);
-            } else {
-                mapBairros.set((obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade), 1);
-            }
+    pedidos_concluidos.forEach(function(obj) {
 
-            // ---------------------------
-            // Gráfico utilizando data de criação do pedido
-            // ---------------------------
-            var tempDate = (new Date(obj.data_criacao.toString().substring(0, 10))).toString();
-            if (mapDatas.get(tempDate)) {
-                var cont = mapDatas.get(tempDate);
-                mapDatas.delete(tempDate);
-                mapDatas.set(tempDate, cont + 1);
-            } else {
-                mapDatas.set(tempDate, 1);
-            }
-        });
+        // ---------------------------
+        // Gráfico utilizando bairro - cidade
+        // ---------------------------
+        if (mapBairros.get(obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade)) {
+            var cont = mapBairros.get(obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade);
+            mapBairros.delete(obj.endereco_entrega.bairro);
+            mapBairros.set(obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade, cont + 1);
+        } else {
+            mapBairros.set((obj.endereco_entrega.bairro + ' - ' + obj.endereco_entrega.cidade), 1);
+        }
 
-        // -----------------------------------------------
-        // Criando dados para colocar no gráfico de pizza
-        // -----------------------------------------------
-        mapBairros.forEach(function(value, key, mapObj) {
-            $scope.dados.push([key, value]);
-        });
+        // ---------------------------
+        // Gráfico utilizando data de criação do pedido
+        // ---------------------------
+        var tempDate = (new Date(obj.data_criacao.toString().substring(0, 10))).toString();
+        if (mapDatas.get(tempDate)) {
+            var cont = mapDatas.get(tempDate);
+            mapDatas.delete(tempDate);
+            mapDatas.set(tempDate, cont + 1);
+        } else {
+            mapDatas.set(tempDate, 1);
+        }
+    });
 
-        // -----------------------------------------------
-        // Criando dados para colocar no gráfico das datas
-        // -----------------------------------------------
-        mapDatas.forEach(function(value, key, mapObj) {
-            $scope.datasDosPedidos.push([new Date(key.toString().substring(0, 15)), value]);
-        });
-    }
+    // -----------------------------------------------
+    // Criando dados para colocar no gráfico de pizza
+    // -----------------------------------------------
+    mapBairros.forEach(function(value, key, mapObj) {
+        $scope.pedService.dados.push([key, value]);
+    });
 
+    // -----------------------------------------------
+    // Criando dados para colocar no gráfico das datas
+    // -----------------------------------------------
+    mapDatas.forEach(function(value, key, mapObj) {
+        $scope.pedService.datasDosPedidos.push([new Date(key.toString().substring(0, 15)), value]);
+    });
 }
